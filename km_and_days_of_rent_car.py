09c37a51190fc10c7f6a6f275b7b0a36795b9bb1
Guilherme Borghezani Pereira Zuchinalli <81888828+NoTzuch@users.noreply.github.com>
2021-09-23 17:02:42 -0300

    Update and rename km_e_dias_de_rent_car.py to km_and_days_of_rent_car.py

---
+++ km_and_days_of_rent_car.py
@@ -0,0 +1,6 @@
+from math import*
+
+km = float(input('Enter the amount of kilometers rotated:'))
+days = float(input('Enter how many rental days:'))
+price = (dias * 60) + (km * 0.15)
+print('O Preço a pagar será de: ' , price)
